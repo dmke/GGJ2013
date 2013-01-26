db1a34217c0d15248d3721799c72b2438b5e4f73
Andreas Bresser <a.bresser@greyrook.com>
2013-01-26 21:47:32 +0100

    obstacles weiter verteilen und Grafik-Gefummel

--- src/custom/myObstacles.lua
+++ src/custom/myObstacles.lua
@@ -1,18 +1,18 @@
 -- moving obstacles
 local crates = {}  
 for i = 1, 80, 1 do
-	crates[i] = display.newImage("images/crate.png")
-	crates[i].x = 800 * i + math.random(100)
+	crates[i] = display.newImage("images/pille.png")
+	crates[i].x = 800 * i + math.random(750)
 	crates[i].y = 200
 	crates[i].name = "power_up"
-	physics.addBody( crates[i], { density = 0.5, friction = 0.1, bounce = 0, radius = 10} )
+	physics.addBody( crates[i], { density = 0.0, friction = 0.0, bounce = 0, radius = 15} )
 	game.blocks:insert(crates[i])
 end
 -- static obstacles (trash can)
 local crates = {}  
 for i = 1, 50, 1 do
 	crates[i] = display.newImage("images/trash.png")
-	crates[i].x = 1000 * i + math.random(100)
+	crates[i].x = 1000 * i + math.random(500)
 	crates[i].y = game.groundLevel - 150
 	crates[i].name = "dynamic"
 	physics.addBody( crates[i], "dynamic", { density = 1.0, friction = 1.2, bounce = 0} )
@@ -23,7 +23,7 @@ end
 local crates = {}  
 for i = 1, 10, 1 do
 	crates[i] = display.newImage("images/laterne.png")
-	crates[i].x = 1000 * i + math.random(98)
+	crates[i].x = 777 * i + math.random(250)
 	crates[i].y = 40
 	crates[i].name = "dynamic"
 	physics.addBody( crates[i], "dynamic", { density = 1.0, friction = 0.9, bounce = 0} )
