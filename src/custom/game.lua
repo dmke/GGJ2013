5308f7747b9e10cb591179f26e6d3091aee9cb31
BorisW <boris.walpert@enthusiastudios.com>
2013-01-27 07:12:29 +0100

    anim speed

--- src/custom/game.lua
+++ src/custom/game.lua
@@ -5,7 +5,6 @@ new = function( params )
     local gameDisplay = display.newGroup()
     display.setStatusBar(display.HiddenStatusBar)
 
-<<<<<<< HEAD
 local physics = require("physics")
 physics.start()
 physics.setGravity(0, 25)
@@ -62,69 +61,6 @@ end
 
 --how many times to call(-1 means forever))
 timer.performWithDelay(1, mainLoop, -1)
-
-local function winConditionCheck( event )
-	time = time + 1;
-    if(health > 0) then
-    	health = health -1
-=======
-    local physics = require("physics")
-    physics.start()
-    physics.setGravity(0, 25)
-    physics.setDrawMode("normal") -- overlays collision outlines on normal Corona objects
-
-    --start background music
-    local player = require("audioPlayer")
-    player.aggressor()
-
-    --setup some variables that we will use to position the ground
-    groundLevel = display.contentHeight --+ display.screenOriginY  FIXME android nenu
-    screenWidth = display.contentWidth
-
-    speed = 5
-    maxHealth = 30
-    health = maxHealth
-    time = 0
-    alive = true
-
-    local scoreText = display.newText("score: ", 0, 0, "badaboom", 60)
-    scoreText:setReferencePoint(display.CenterLeftReferencePoint)
-    scoreText.x = 60
-    scoreText.y = 20
-
-    local timeText = display.newText("time: ", 0, 0, "badaboom", 60)
-    timeText:setReferencePoint(display.CenterLeftReferencePoint)
-    timeText.x = 400
-    timeText.y = 20
-
-    --create a new group to hold all of our physics objects
-    blocks = display.newGroup()
-    particles = display.newGroup()
-
-    require("myBackground")
-    require("hearty")
-    require("myObstacles")
-
-    print(display.screenOriginX)
-
-    function mainLoop()
-        if(alive) then
-            updateHero()
-            local speed = hero:getLinearVelocity()
-            --print("speed " .. speed)
-
-            timeText.text = "time: " .. time
-            scoreText.text = "health: " .. health
-
-            updateMyBackground(speed/10)
-            blocks.x = -hero.x + screenWidth/4
-            particles.x = -hero.x + 320
-        end
->>>>>>> 88874a942214ef236b1fe2d6ab8b2d0e558bac09
-    end
-
-    timer.performWithDelay(1, mainLoop, -1)
-
     local function winConditionCheck( event )
         time = time + 1;
         if health > 0 then
